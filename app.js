b2838dcd2a517d9b66a65667e73e4c5afc753193
duyleomessi <duyleitbka95@gmail.com>
2017-11-19 22:23:40 +0700

    observer pattern

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 var path = require('path');
 var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
 
 var componentPath = path.join(__dirname, "component");
-fs.readdirSync(componentPath).forEach(function(dir) {
-    fs.readFile('./component/' + dir + '/package.json', 'utf8', function(err, data) {
-        if(err) {
-            console.log("Error while loading component!!!");
-        } else {
-            var infor = JSON.parse(data);
-            require('./component/' + dir + '/' + infor['main']);
-        }
+
+function findFile() {
+    var emitter = new EventEmitter();
+    fs.readdirSync(componentPath).forEach(function (dir) {
+        fs.readFile('./component/' + dir + '/package.json', 'utf8', function (err, data) {
+            if (err) {
+                return emitter.emit('error', err);
+            } else {
+                var infor = JSON.parse(data);
+                if (infor['main'] != null) {
+                    var pathFile = './component/' + dir + '/' + infor['main'];
+                    emitter.emit('found', pathFile);
+                } else {
+                    emitter.emit('fileNotFound');
+                }
+                //require('./component/' + dir + '/' + infor['main']);
+            }
+        })
     })
-}) 
+    return emitter;
+}
+
+findFile()
+    .on('found', function (pathFile) {
+        require(pathFile);
+    })
+    .on('fileNotFound', function() {
+        console.log("File not found for main in the package.json");
+    })
+    .on('error', function() {
+        console.log("Error while loading component!!!");
+    })
